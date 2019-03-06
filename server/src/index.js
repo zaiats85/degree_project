118ba9cc7fc8ebf90535b1251490993c2475d56b
Oleg Zaiats <ozaiats@ozaiats.local>
2019-03-06 18:09:05 +0200

    mutations

--- server/src/index.js
+++ server/src/index.js
@@ -1,51 +1,133 @@
-const { GraphQLServer } = require('graphql-yoga')
+const {GraphQLServer} = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
+let members = [
+    {
+        name: "Oleg",
+        age: 33,
+        budget: 100000,
+    },
+    {
+        name: "Dariya",
+        age: 30,
+        budget: 200000,
+    },
+    {
+        name: "Jan",
+        age: 27,
+        budget: 10,
+    },
+    {
+        name: "Arcadiy",
+        age: 48,
+        budget: 4210000,
+    },
+    {
+        name: "Vladimir",
+        age: 60,
+        budget: 1,
+    },
+    {
+        name: "Oksana",
+        age: 22,
+        budget: 1000000,
+    }
+];
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.kpi.ua',
+        description: 'best education ever',
+        members: [
+            {
+                name: "Oleg",
+                age: 33,
+                budget: 100000,
+            },
+            {
+                name: "Dariya",
+                age: 30,
+                budget: 200000,
+            },
+        ]
 
-type Mutation {
-  post(url: String!, description: String!): Link!
-}
+    },
+    {
+        id: 'link-1',
+        url: 'www.lpi.ua',
+        description: 'worst education ever',
+        members: [
+            {
+                name: "Jan",
+                age: 27,
+                budget: 10,
+            },
+            {
+                name: "Arcadiy",
+                age: 48,
+                budget: 4210000,
+            }
+        ]
+    },
+    {
+        id: 'link-2',
+        url: 'www.kiokyshin.com',
+        description: 'Andy Hug',
+        members: [
+            {
+                name: "Vladimir",
+                age: 60,
+                budget: 1,
+            },
+            {
+                name: "Oksana",
+                age: 22,
+                budget: 1000000,
+            }
+        ]
+    }
+];
 
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
+let idCount = links.length;
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-// 1
-let idCount = links.length
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`,
+        info: () => `This is the API of Hackernews WhatEver Clone`,
         feed: () => links,
+        people: () => members,
+        link: (parent, args) => links.find(item => item.id === args.id)
     },
+
     Mutation: {
-        // 2
+        updateLink: (parent, args) => {
+            return links.map(item => {
+                if(item.id === args.id){
+                    item.description = args.description
+                    item.url = args.url
+                }
+                return item
+            });
+        },
+
+        deleteLink: (parent, args) => {
+            links = links.filter(item => item.id !== args.id)
+        },
+
         post: (parent, args) => {
             const link = {
                 id: `link-${idCount++}`,
                 description: args.description,
                 url: args.url,
-            }
-            links.push(link)
+            };
+            links.push(link);
             return link
         }
     },
-}
+};
+
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers,
+    resolvers
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`));
